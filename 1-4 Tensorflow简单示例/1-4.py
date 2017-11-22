da251797c39ae2541e080b49464175f2825cd2c5
Wang <32231771+wanghanyang123@users.noreply.github.com>
2017-11-22 09:55:06 +0800

    v1

---
+++ 1-4 Tensorflow简单示例/1-4.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+
+#使用numpy产生100个随机点
+x_data=np.random.rand(100)
+y_data=x_data*0.1+0.2
+
+#构造一个线性模型
+b=tf.Variable(0.)
+k=tf.Variable(0.)
+y=k*x_data+b
+
+#构建二次代价函数
+loss=tf.reduce_mean(tf.square(y_data-y))
+#定义一个梯度下降法来进行训练优化器，括号里的值是学习率
+optimizer=tf.train.GradientDescentOptimizer(0.1)
+#定义一个最小化代价函数
+train=optimizer.minimize(loss)
+#初始化变量
+init=tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(201):
+        sess.run(train)
+        if step%20==0:
+            print(step,sess.run((k,b)))
